2c23a50ff637ea561ec309f316ec12810a203423
Tenari <djz24@case.edu>
2013-09-26 09:39:06 -0400

    dashboard work

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -1,3 +1,12 @@
+Template.dashboard.noChallenges = function(){
+  return Bets.find({placer: Meteor.userId()}).count() == 0;
+};
+Template.dashboard.invites = function(){
+  return []; // need to actually model this.
+};
+Template.dashboard.showInvites = function(){
+  return Session.get('dash-showInvites');
+};
 Template.dashboard.bets = function(){
   return Bets.find({friend: Meteor.user().profile.name});
 };
@@ -11,5 +20,12 @@ Template.dashboard.events({
       Session.set('dash-showBets',false);
     else
       Session.set('dash-showBets',true);
+  },
+  'click #showInvites': function(){
+    var show = Session.get('dash-showInvites');
+    if(show)
+      Session.set('dash-showInvites', false);
+    else
+      Session.set('dash-showInvites', true);
   }
 });
