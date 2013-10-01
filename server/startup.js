ad080f8d78d6bb795120479c857c9786723d0ffa
Tenari <djz24@case.edu>
2013-10-01 19:37:01 -0400

    fix: FB config

--- server/startup.js
+++ server/startup.js
@@ -1,13 +1,13 @@
 Meteor.startup(function(){
   console.log(process.env.ROOT_URL);
-  if (process.env.ROOT_URL.indexOf("localhost") != -1){ // we aren't on developpment
+  if (process.env.ROOT_URL !="http://localhost:3000"){ // we aren't on developpment
     // first, remove configuration entry in case service is already configured
     /*Accounts.loginServiceConfiguration.remove({
       service: "facebook"
     });*/
     Accounts.loginServiceConfiguration.insert({
       service: "facebook",
-      clientId: "178891345633576",
+      appId: "178891345633576",
       secret: "dac634e282438dcdfa58f30b3a62aa4a"
     });
   }
