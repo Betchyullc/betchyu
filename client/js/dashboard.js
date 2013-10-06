33fa61f3b8f0926e1ace3c4e2248ee92a23d33d5
Tenari <djz24@case.edu>
2013-10-05 22:37:43 -0400

    STYLE: dashboard buttons + amount indicator

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -2,3 +2,6 @@ Template.dashboard.fbPicURL = Meteor.shared.fbPicURL;
 Template.dashboard.noChallenges = function(){
   return Bets.find({placer: Meteor.userId()}).count() == 0;
 };
+Template.dashboard.amountOffered = function(){
+  return Invites.find({invitee: Meteor.userId(), declined:false, accepted: false}).count();
+};
