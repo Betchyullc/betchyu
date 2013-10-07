e0f1840dfc4cdf11a0708db79b3f1c287dc31583
Tenari <djz24@case.edu>
2013-10-07 10:38:59 -0400

    step two re-vamping

--- client/js/step_two.js
+++ client/js/step_two.js
@@ -1,13 +1,26 @@
-Template.step_two.helpers({
-  selected: function(name){
-    if (name == Session.get('product'))
-      return  'product-pick';
-    return '';
-  }
-});
+Template.step_two.credit_card = function(){
+  return 'in progress';
+};
+Template.step_two.win_amt = function(){
+  return Session.get('product').value;
+};
+Template.step_two.lose_amt = function(){
+  return (Session.get('product').value * 1.1).toFixed(2);
+};
+Template.step_two.selected = function(){
+  return Session.get('product') != undefined;
+};
+
+// EVENTS
 Template.step_two.events({
-  'click li': function(e){
-    var product = $(e.target).text();
-    Session.set('product', product);
+  'change .stake-amount': function(e){
+    var tar = $(e.target);
+    var val = tar.val();
+    var prod = tar.data('product');
+    Session.set('product', {
+      product: prod,
+      value: val
+    });
+
   }
 });
