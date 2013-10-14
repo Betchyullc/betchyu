170e8130636107401ed11da14bbccc6506f4d1a3
Tenari <djz24@case.edu>
2013-10-14 08:35:15 -0400

    edit: step_two to be non-gift card

--- client/js/step_two.js
+++ client/js/step_two.js
@@ -1,3 +1,4 @@
+/*
 Template.step_two.credit_card = function(){
   var c_i = Session.get('customer-info');
   if (c_i){
@@ -20,14 +21,21 @@ Template.step_two.win_amt = function(){
 };
 Template.step_two.lose_amt = function(){
   return (Session.get('product').value * 1.1).toFixed(2);
-};
+};*/
 Template.step_two.selected = function(){
   return Session.get('product') != undefined;
+};
+Template.step_two.isSelected = function(thing){
+  if ( Session.get('product') != undefined
+       && Session.get('product').product == thing ) {
+    return "product-pick";
+  }
+  return "";
 };
 
 // EVENTS
 Template.step_two.events({
-  'change .stake-amount': function(e){
+  /*'change .stake-amount': function(e){
     var tar = $(e.target);
     var val = tar.val();
     var prod = tar.data('product');
@@ -35,7 +43,25 @@ Template.step_two.events({
       product: prod,
       value: val
     });
-
+  }*/
+  'click li': function(e){
+    var tar = $(e.target);
+    var val = tar.text();
+    Session.set('product', {
+      product: val,
+      value: 10
+    });
+  },
+  'change #stake': function(e){
+    var val = $('#stake').val();
+    var prod = Session.get('product').product;
+    Session.set('product', {
+      product: prod,
+      value: val
+    });
+  },
+  'click #stake': function(e){
+    e.stopPropagation();
   }
 });
 Template.step_two.created = function(){ Meteor.shared.logPageView("step_two");};
