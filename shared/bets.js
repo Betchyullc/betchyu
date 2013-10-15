7d95d4da04503ff44d66d1f9b8911d66ae68b7e7
Tenari <djz24@case.edu>
2013-10-14 22:56:57 -0500

    commenting on bet format

--- shared/bets.js
+++ shared/bets.js
@@ -1,4 +1,26 @@
 Bets = new Meteor.Collection("bets");
+/*
+  {
+    _id: "hash",
+    createdAt: "timestamp",
+    days: number,
+    friends: [
+      {
+        id: "facebook id",
+        name: "Joe Smith"
+      }, {} ...
+    ],
+    goal: {
+      type: "code",
+      value: number
+    },
+    placer: "userId hash",
+    product: {
+      product: "a thing",
+      value: number
+    }
+  }
+*/
 Bets.allow({
   insert: function(userId, bet){
     return userId 
