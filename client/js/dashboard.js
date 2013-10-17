d8e08a2481bfdfef03f2bf549a6dbb8c1c71dedb
Tenari <djz24@case.edu>
2013-10-17 11:41:34 -0400

    update progress: started

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -45,8 +45,22 @@ Template.dashboard.bet_progress = function(bet){
       return "<p>How many workouts have you done so far?</p>"+
              "<input type='text' class='dash-input'/>"; 
     case "run":
-      return "<p>How many miles have you run so far?</p>"+
-             "<input type='text' class='dash-input'/>"; 
+      var now = new Date();
+      var then = new Date(bet.createdAt);
+      var days_passed = parseInt( (now - then)/1000/60/60/24);
+      var inputHTML = "";
+      for (var i = 0; i < days_passed; i++){
+        now.setDate(then.getDate()+i);
+        if( bet.update  && bet.update.done_each_day[i]){
+          inputHTML += "<input title='"+now.toDateString()+"' type='text' class='dash-input' value='"+bet.update.done_each_day[i]+"'/>";
+        } else{
+          inputHTML += "<input title='"+now.toDateString()+"' type='text' class='dash-input'/>";
+        }
+      }
+      return "<p>How many miles have you run each day?</p>"+
+             "<div class='dash-goal-days'>"+
+               inputHTML+
+             "</div>";
     case "lbs":
     default:
       return "<p>How many pounds have you lost so far?</p>"+
@@ -73,15 +87,19 @@ Template.dashboard.events({
     });
   },
   'click #updateBetInfo': function(){
-    var done_so_far = $('.dash-input').val();
     var betId = $('.block-content').data('_id');
+    var updates = [];
+    $('.dash-input').each(function(i, e){
+      updates.push($(this).val());
+    });
 
     Bets.update(betId, {$set:{
       update: {
         updatedAt: new Date().getTime(),
-        done_so_far: done_so_far
+        done_each_day: updates
       }
     }});
+
   }
 });
 
