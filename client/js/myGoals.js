74839f97df41004a4ac30fdbfe9bb69999009538
Tenari <djz24@case.edu>
2013-10-17 12:34:14 -0400

    redo: myGoals page is like wireframe

---
+++ client/js/myGoals.js
@@ -0,0 +1,9 @@
+Template.myGoals.bets = function() {
+  return Bets.find({placer: Meteor.userId()});
+};
+Template.myGoals.invites = function(betId){
+  return Invites.find({bet:betId, uncreated:{$not: true}});
+};
+Template.myGoals.bet_name = Meteor.shared.bet_name;
+Template.myGoals.days_left = Meteor.shared.days_left;
+Template.myGoals.created = function(){ Meteor.shared.logPageView("myGoals");};
