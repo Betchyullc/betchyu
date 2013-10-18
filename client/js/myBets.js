2174364a0a7d1204439a90b15cd0e9c3b4a0df62
Tenari <djz24@case.edu>
2013-10-17 23:07:43 -0400

    myBets page is made

--- client/js/myBets.js
+++ client/js/myBets.js
@@ -1,9 +1,42 @@
-Template.myBets.bets = function() {
-  return Bets.find({placer: Meteor.userId()});
+Template.myBets.acceptedBets = function(){
+  return Invites.find({invitee: Meteor.userId(), declined:false, accepted: true});
 };
-Template.myBets.invites = function(betId){
-  return Invites.find({bet:betId, uncreated:{$not: true}});
+Template.myBets.offeredBets = function(){
+  return Invites.find({invitee: Meteor.userId(), declined:false, accepted: false});
 };
-Template.myBets.bet_name = Meteor.shared.bet_name;
+Template.myBets.fbPicURL = Meteor.shared.fbPicURL;
 Template.myBets.getName = Meteor.shared.getName;
+Template.myBets.bet_name = function(invite_obj){
+  return Meteor.shared.bet_name(Bets.findOne(invite_obj.bet));
+};
+Template.myBets.days_left = function(invite){
+  return Meteor.shared.days_left(Bets.findOne(invite.bet));
+};
+Template.myBets.pendingBets = function(){
+  return Template.myBets.offeredBets().count() > 0;
+};
+
+Template.myBets.events({
+  'click .acceptBet': function(e){
+    var invId = $(e.target).data('invite');
+
+    Invites.update(invId, {
+      $set: {
+        accepted: true,
+        declined: false
+      }
+    });
+  },
+  'click .declineBet': function(e){
+    var invId = $(e.target).data('invite');
+
+    Invites.update(invId, {
+      $set: {
+        accepted: false,
+        declined: true
+      }
+    });
+  }
+});
+
 Template.myBets.created = function(){ Meteor.shared.logPageView("myBets");};
