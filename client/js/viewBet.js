e91789702f625127cbcb2015b04ca2d0b493dafb
Tenari <djz24@case.edu>
2013-10-18 11:07:25 -0400

    viewBets: mostly done, update implies win condition/no more buttons for win/lose

--- client/js/viewBet.js
+++ client/js/viewBet.js
@@ -1,3 +1,4 @@
+//Boolean helpers
 Template.viewBet.isAccepted = function(){
   var bet_obj = Bets.findOne(Session.get('bet'));
   var invs = Invites.find({bet: bet_obj._id}); // all the invites for this bet.
@@ -7,13 +8,34 @@ Template.viewBet.isAccepted = function(){
   if (invite == undefined) return true;
   return invite.accepted;
 };
-Template.viewBet.bet = function(){
-  return Bets.find(Session.get('bet'));
+Template.viewBet.need_bet_update = function(){
+  var bet = Bets.findOne(Session.get('bet'));
+
+  if (bet.update){
+    var timestamp = ((new Date().getTime())-(1000*60*60*24)); // 1 day
+    return bet.update.updatedAt < timestamp;
+  } else {
+    return true;
+  }
 };
 Template.viewBet.needToInvite = function(fbid){
   if (Meteor.userId() != Bets.findOne(Session.get('bet')).placer)
     return false;
   return Meteor.users.findOne({"services.facebook.id":fbid}) == undefined;
+};
+Template.viewBet.accepted = function(betId){
+  return Invites.find({bet: betId, accepted: true, declined:false});
+};
+Template.viewBet.isFinished = function(){
+  return _.isString(Bets.findOne(Session.get('bet')).winner);
+};
+Template.viewBet.isOwner = function(){
+  return Bets.findOne(Session.get('bet')).placer == Meteor.userId();
+};
+
+// object/string returning helpers
+Template.viewBet.bet = function(){
+  return Bets.findOne(Session.get('bet'));
 };
 Template.viewBet.formatted_owners_bet = function(){
   var bet_obj = Bets.findOne(Session.get('bet'));
@@ -23,31 +45,22 @@ Template.viewBet.formatted_owners_bet = function(){
   return owner_name + "'s Bet";
 };
 Template.viewBet.days_left = Meteor.shared.days_left;
-Template.viewBet.accepted = function(betId){
-  return Invites.find({bet: betId, accepted: true, declined:false});
-};
 Template.viewBet.getName = function(userId){
   return Meteor.users.findOne(userId).profile.name;
 };
-Template.viewBet.isFinished = function(){
-  return _.isString(Bets.findOne(Session.get('bet')).winner);
-};
 Template.viewBet.resultOfBet = function(){
-  var win = '<span style="color:green">You Won This Bet!</span>',
-      lose = '<span style="color:red">You Lost This Bet...</span>',
+  var win = '<span style="color:green">You Won!</span>',
+      lose = '<span style="color:red">You Lost...</span>',
       bet = Bets.findOne(Session.get('bet'));
   if (bet.placer == Meteor.userId()){
     if(bet.winner == 'placer')
       return win;
     return lose;
   }else{
-    if(bet.winner != 'placer')
+    if(bet.winner == 'placer')
       return lose;
     return win;
   }
-};
-Template.viewBet.isOwner = function(){
-  return Bets.findOne(Session.get('bet')).placer == Meteor.userId();
 };
 Template.viewBet.the_bet = function(){
   var bet = Bets.findOne(Session.get('bet'));
@@ -63,21 +76,150 @@ Template.viewBet.the_bet = function(){
       return "Lose "+bet.goal.value+" lbs in "+bet.days+" days"; 
   }
 };
+var makeInputHTML = function(bet){
+  var now = new Date();
+  var then = new Date(bet.createdAt);
+  var days_passed = Math.ceil( (now - then)/1000/60/60/24);
+  var inputHTML = "";
+  for (var i = 0; (i < days_passed && i < bet.days); i++){
+    now.setDate(then.getDate()+i);
+    if( bet.update  && bet.update.done_each_day[i]){
+      inputHTML += "<input title='"+now.toDateString()+"' type='text' class='dash-input' value='"+bet.update.done_each_day[i]+"'/>";
+    } else{
+      inputHTML += "<input title='"+now.toDateString()+"' type='text' class='dash-input'/>";
+    }
+  }
+  return inputHTML;
+}
+Template.viewBet.bet_progress = function(bet){
+  var inputHTML = makeInputHTML(bet);
+  inputHTML ="<div class='dash-goal-days'>"+
+               inputHTML+
+             "</div>";
+  switch(bet.goal.type){
+    case "calories":
+      return "<p>How many calories have you eaten each day?</p>"+
+             inputHTML;
+    case "workout":
+      return "<p>How many workouts have you done each day?</p>"+
+               inputHTML;
+    case "run":
+      return "<p>How many miles have you run each day?</p>"+
+               inputHTML;
+    case "lbs":
+    default:
+      return "<p>How many pounds have you lost each day?</p>"+
+               inputHTML;
+  }
+};
+Template.viewBet.days_from_bet = function(bet){
+  var now = new Date();
+  var start = new Date(bet.createdAt);
+  var elem;
+  switch(bet.goal.type){
+    case "calories":
+      elem = " Calories Eaten";
+      break;
+    case "workout":
+      elem = " workouts done";
+      break;
+    case "run":
+      elem = " miles run";
+      break;
+    case "lbs":
+    default:
+      elem = " lbs lost";
+      break;
+  }
+  var returnHTML = "<ul>";
+  _.each(bet.update.done_each_day, function(e, i, l){
+    var thisDay = new Date(start.getTime() + (i*24*60*60*1000));
+    returnHTML += "<li><strong>"+thisDay.toDateString()+":</strong>"+e+elem+"</li>";
+  });
+  returnHTML += "</ul>";
+
+  return returnHTML;
+};
 
 Template.viewBet.created = function(){ Meteor.shared.logPageView("viewBet");};
+
+var winTheBet = function(bet){
+  Bets.update(Session.get('bet'), {
+    $set: { winner: 'placer' }
+  });
+};
+var loseTheBet = function(bet){
+  Bets.update(Session.get('bet'), {
+    $set: { winner: 'friends' }
+  });
+};
 
 Template.viewBet.events({
   'click #addPeopleToBet': function(){
     Session.set('view', 'add friends to bet');
   },
-  'click #winBet':function(){
-    Bets.update(Session.get('bet'), {
-      $set: { winner: 'placer' }
+  'click #updateBetInfo': function(){
+    var betId = $('.block-content').data('_id');
+    var updates = [];
+    var valid = true;
+    $('.dash-input').each(function(i, e){
+      var update = $(this).val().trim();
+      if(!isNaN(update)){
+        updates.push(update);
+      } else {
+        valid = false;
+      }
     });
-  },
-  'click #loseBet':function(){
-    Bets.update(Session.get('bet'), {
-      $set: { winner: 'friends' }
-    });
+    
+    if (valid){
+      Bets.update(betId, {$set:{
+        update: {
+          updatedAt: new Date().getTime(),
+          done_each_day: updates
+        }
+      }});
+
+      // determine if they finished the bet
+      var bet = Bets.findOne(betId);
+      var bet_total = 0;
+      _.each(updates, function(e, i, l){
+        bet_total += e;
+      });
+      var now = new Date().getTime();
+      var then = new Date(bet.createdAt).getTime();
+      var betIsOver = ((now - (bet.days*24*60860*1000)) > then);
+      switch(bet.goal.type){
+        case "calories":
+          if ((bet.goal.value * bet.days) >= bet_total)
+            winTheBet(bet);
+          else if (betIsOver)
+            loseTheBet(bet);
+          break;
+        case "workout":
+          if (bet.goal.value >= bet_total)
+            winTheBet(bet);
+          else if (betIsOver)
+            loseTheBet(bet);
+          break;
+        case "run":
+          if ((bet.goal.value * bet.days) >= bet_total)
+            winTheBet(bet);
+          else if (betIsOver)
+            loseTheBet(bet);
+          break;
+        case "lbs":
+        default:
+          if (bet.goal.value >= bet_total)
+            winTheBet(bet);
+          else if (betIsOver)
+            loseTheBet(bet);
+          break;
+      }
+    }
   }
 });
+Template.viewBet.rendered = function(){
+  if (!Template.viewBet.need_bet_update()){
+    $('.dash-update-hook').delay(1500).slideUp( 600);
+  }
+}
